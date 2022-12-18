44eedcceba555881d1170779dd06e341405646a2
AmineKotti <mohamedamine.kotti@esprit.tn>
2022-12-18 14:13:51 +0100

    shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
 import HomePage from './pages/homepage/homepage.component'
+import {  Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <ShopPage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>  
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import './menu-item.styles.scss';
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`} >
+const MenuItem = ({ title,size, imageUrl, history, linkUrl, match }) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}` )} >
       <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}}></div>
       <div className="content">
          <h1 className="title">{title.toUpperCase()}</h1>
-         <span className="subtitle">ZZ NOW</span>
+         <span className="subtitle">Shop</span>
       </div>
     </div>
 );
 
 
-export default MenuItem;
+export default withRouter(MenuItem);
